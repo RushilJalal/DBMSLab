253250281fa5610bfbc20566f1dc8cde1a25cdc3
Rushil Jalal <rushiljalal@gmail.com>
2024-02-01 16:14:54 +0530

    Update l3q1.sql

--- lab3/l3q1.sql
+++ lab3/l3q1.sql
@@ -21,12 +21,10 @@ create table accident(
     location varchar(50),
     primary key(report_number));
 
-
 create table owns(
     driver_id varchar(30),
     regno varchar(20));
     primary key(driver_id, regno);
-
 
 create table participated(
 driver_id varchar(30),
@@ -43,30 +41,37 @@ insert into person values('2','Mary','Pune');
 insert into person values('3','Sasha','New York');
 insert into person values('4','Anaadhi','Delhi');
 insert into person values('5','Mongia','Delhi');
+insert into person values('6','JFk','Texas');
 
 insert into car values('A', 'model1',2000);
 insert into car values('B', 'model2',2001);
 insert into car values('C', 'model3', 2002);
 insert into car values('D', 'model4', 2003);
 insert into car values('E', 'model5', 2004);
+insert into car values('F', 'model1', 2007);
+insert into car values('240a', 'toyota', 2020);
 
 insert into accident values(12, To_Date('2024/01/01', 'yyyy/mm/dd'), 'Ghaziabad');
 insert into accident values(13, To_Date('2024/02/01', 'yyyy/mm/dd'), 'Ghaziabad');
 insert into accident values(14, To_Date('2024/03/01', 'yyyy/mm/dd'), 'Ghaziabad');
 insert into accident values(15, To_Date('2024/04/01', 'yyyy/mm/dd'), 'Ghaziabad');
 insert into accident values(16, To_Date('2024/05/01', 'yyyy/mm/dd'), 'Ghaziabad');
+insert into accident values(17, To_Date('2008/05/01', 'yyyy/mm/dd'), 'Lucknow');
 
 insert into owns values('1','A');
 insert into owns values('2','B');
 insert into owns values('3','C');
 insert into owns values('4','D');
 insert into owns values('5','E');
+insert into owns values('6','F');
+insert into owns values(123456, '240a');
 
 insert into participated values('1', 'A', 12, 10000);
 insert into participated values('2', 'B', 13, 20000);
 insert into participated values('3', 'C', 14, 3000);
 insert into participated values('4', 'D', 15, 50000);
 insert into participated values('5', 'E', 16, 990000);
+insert into participated values('6', 'F', 17, 3469);
 
 update participated set damage_amount = 25000 where regno = 'A' and report_number = 12;
 
